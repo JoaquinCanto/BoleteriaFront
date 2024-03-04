afcee4c12515c91486cde80e1892fdbd26df5db3
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-04 20:46:21 -0300

    Modificados Header y Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,14 @@
+import './Footer.css'
+
 export function Footer(){
 	return(
-		<footer className="">
-			<div>
-				<div className="follow">
-					<div>Seguinos:</div>
-					<div>
-						{/* <span><a href=""><img class="socImage" src="Images/f.png" alt=""></a></span>
-						<span><a href=""><img class="socImage" src="Images/i.png" alt=""></a></span>
-						<span><a href=""><img class="socImage" src="Images/t.png" alt=""></a></span>  */}
-					</div>
-				</div>
-				<div className="legales">
-					<div>Legales</div>
-					<div>
-						<span><a href="">Terminos y Condiciones</a></span>
-						<span><a href="">Política de Privacidad</a></span>
-					</div>
-				</div>
+		<footer>
+			<div className="legales">
+				<span><a href="">Terminos y Condiciones</a></span>
+				<span><a href="">Política de Privacidad</a></span>
 			</div>
 			<div className="derechos">
-				Todos Los Derechos Reservados © 2023 Canto Inc.
+				Todos Los Derechos Reservados © 2024 Canto Inc.
 			</div>
 		</footer>
 	)
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
 import './Header.css'
+import logo from "../../assets/EventEntry.svg"
+import user from "../../assets/User.svg"
 
 export function Header(){
 	return(
 		<header className="">
-			<div className="logoNombre">
-				<img src='' alt="Logo"></img>
-				<span>EventEntry</span>
-			</div>
-			<div className="separator"></div>
+			{/* <div className="logo"> */}
+				<img className="logo" src={logo} alt="LogoEventEntry"></img>
+			{/* </div> */}
 			<nav className="">
 				<ul>
-					<li>Enventos</li>
+					<li>Eventos</li>
+					<div className="vLine"></div>
 					<li>Sedes</li>
+					<div className="vLine"></div>
 					<li>Nosotros</li>
+					<div className="vLine"></div>
 					<li>Soporte</li>
 				</ul>
 			</nav>
+			<div >
+				<img className="usuario" src={user} alt="usuario"></img>
+			</div>
 		</header>
 	)
 }

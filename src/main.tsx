aff43dc2ba25ad4e25c1f7dd23f2c7f59960e2f4
zequ22 <eze22alderete@gmail.com>
2024-03-04 20:55:35 -0300

    Campos pertenecientes a Usuarios

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import Login from './components/Usuarios/LogIn/LogIn.tsx'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>,
 )

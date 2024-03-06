2d599c381bf41e493ceaf2a4cffbc273502c56ce
zequ22 <eze22alderete@gmail.com>
2024-03-06 15:06:46 -0300

    Soporte

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import Nosotros from './components/Nosotros/Nosotros.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Nosotros />
+    <App />
   </React.StrictMode>,
 )

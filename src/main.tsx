65dfecdaf5a7f91300604b55434fbb52e530327a
zequ22 <eze22alderete@gmail.com>
2024-03-06 12:54:22 -0300

    Sedes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import Gigante from './components/Sedes/Gigante/Gigante.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Gigante />
   </React.StrictMode>,
 )

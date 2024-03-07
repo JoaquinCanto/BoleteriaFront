bffc850db263d113e5495ce8bc285fa1da7796a2
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-07 01:57:58 -0300

    Reorganizacion de archivos.

--- src/components/Sedes/Gigante/Gigante.tsx
+++ src/components/Sedes/Gigante/Gigante.tsx
@@ -1,28 +1,28 @@
 import './Gigante.css'
-import FotoGigante from './Gigante.jpg'
-import AccGigante from './Esquema.png'
+import FotoGigante from '../../../assets/Gigante.jpg'
+import GiganteAccesos from '../../../assets/GiganteAccesos.png'
 
 function Gigante() {
 	return (
 	<>
         <div className='Sede1'>
             <h2>Gigante de Arroyito</h2>
-            <img src={FotoGigante} alt="" />
+            <img src={FotoGigante} alt="FotoGigante" />
             <h3>A cerca de</h3>
             <p>El Estadio Gigante de Arroyito es una cancha de fútbol que se encuentra en la ciudad de Rosario a orillas del Río Paraná, en la Provincia de Santa Fe. Su propietario es el Club Atlético Rosario Central.
                 Fue inaugurado oficialmente el 14 de noviembre de 1926 y a través de numerosas reformas (la última en el año 2024), logró alcanzar la actual capacidad estimada de 50.000 espectadores.
                 La FIFA lo designó como una de las sedes de la Copa Mundial de Fútbol de 1978, disputada en Argentina. Esto, lo convierte en el único estadio de la provincia de Santa Fe que ha albergado a una Copa Mundial de Fútbol de mayores.
                 A su vez, el Gigante de Arroyito fue una de las subsedes de la Copa América 1987, disputada en la Argentina.</p>
             <h3>Calles</h3>
-            <p>El Gigante está ubicado en el Barrio Arroyito, en la zona norte de la ciudad de Rosario.<br />
-                El estadio, está circunscrito por las siguientes calles:<br />
-                - Avenida Costanera al norte.<br />
-                - Calle J.B. Cordiviola al oeste.<br />
-                - Avenida Centenario Club Atlético Rosario Central al este.<br />
-                - Avenida Génova al sur. <br />
-                Existe una amplia disponibilidad de transporte público, donde destacan las siguientes líneas de colectivos: 35/9, 102, 103, 107, 110, 113, 129 y 153.</p>
+            <p>El Gigante está ubicado en el Barrio Arroyito, en la zona norte de la ciudad de Rosario.</p>
+                <p>El estadio, está circunscrito por las siguientes calles:</p>
+                <p>- Avenida Costanera al norte.</p>
+                <p>- Calle J.B. Cordiviola al oeste.</p>
+                <p>- Avenida Centenario Club Atlético Rosario Central al este.</p>
+                <p>- Avenida Génova al sur.</p>
+                <p>Existe una amplia disponibilidad de transporte público, donde destacan las siguientes líneas de colectivos: 35/9, 102, 103, 107, 110, 113, 129 y 153.</p>
             <h3>Accesos</h3>
-            <img src={AccGigante} alt="" />
+            <img src={GiganteAccesos} alt="GiganteAccesos" />
         </div>
 	</>
 	)
--- src/components/Sedes/Monumental/Monumental.tsx
+++ src/components/Sedes/Monumental/Monumental.tsx
@@ -1,22 +1,22 @@
 import './Monumental.css'
-import FotoMonumental from './Monumental.jpg'
-import AccMonumental from './Accesos.png'
+import FotoMonumental from '../../../assets/Monumental.jpg'
+import MonumentalAccesos from '../../../assets/MonumentalAccesos.png'
 
 function Monumental() {
 	return (
 	<>
         <div className='Sede1'>
-            <h2>Mâs Monumental</h2>
+            <h2>Más Monumental</h2>
             <img src={FotoMonumental} alt="" />
             <h3>A cerca de</h3>
             <p>El estadio Monumental, también conocido como el Monumental de Núñez, estadio River Plate, o por cuestiones de patrocinio estadio Mâs Monumental, es un recinto deportivo ubicado en la intersección de las avenidas Figueroa Alcorta y Udaondo del barrio de Belgrano, Buenos Aires, Argentina.5​6​ Es propiedad del Club Atlético River Plate y fue inaugurado el 26 de mayo de 1938 por el presidente Antonio Vespucio Liberti, quien decidió su construcción.
                 Cuenta con una capacidad de 84 567 espectadores, luego de las remodelaciones concluidas en 2023.
                 Es el estadio de fútbol con mayor capacidad de Argentina y de América. Es el recinto donde Argentina disputa sus partidos de local. Fue sede de cuatro finales de Copa América (1946, 1959, 1987 y 2011), y sede de la final de la Copa Mundial de la FIFA 1978, entre otros eventos trascendentes.</p>
             <h3>Calles</h3>
-            <p>El recinto deportivo está ubicado en la intersección de las avenidas Figueroa Alcorta y Udaondo del barrio de Belgrano, Buenos Aires, Argentina. <br />
-                Las líneas de colectivo que llegan al estadio Mas Monumental son las siguientes: 15, 29, 37, 42, 57, 107, 130, 152 y 160.</p>
+            <p>El recinto deportivo está ubicado en la intersección de las avenidas Figueroa Alcorta y Udaondo del barrio de Belgrano, Buenos Aires, Argentina. </p>
+            <p> Las líneas de colectivo que llegan al estadio Mas Monumental son las siguientes: 15, 29, 37, 42, 57, 107, 130, 152 y 160.</p>
             <h3>Accesos</h3>
-            <img src={AccMonumental} alt="" />
+            <img src={MonumentalAccesos} alt="" />
         </div>
 	</>
 	)

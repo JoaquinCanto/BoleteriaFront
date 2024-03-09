b9fe185665a9eec6eff10ed6076b1e0ffccbbed6
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-09 05:53:11 -0300

    Reestructuracion de Register y autenticacion con Firebase.

--- src/components/Usuarios/Register.tsx
+++ src/components/Usuarios/Register.tsx
@@ -1,48 +1,94 @@
 import './Usuario.css'
-import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useRef } from 'react'
+import { useForm } from 'react-hook-form';
+import { auth } from '../../helpers/firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Registro: React.FC = () => {
-    const [formulario, setFormulario] = useState({
-        nombre: '',
-        apellido: '',
-        fechaNacimiento: '',
-        dni: '',
-        telefono: '',
-        correo: '',
-        contraseña: '',
-        repetirContraseña: ''
-    });
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFormulario({
-            ...formulario,
-            [event.target.name]: event.target.value
-        });
-    };
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Aquí puedes realizar cualquier acción necesaria con el formulario enviado
-    };
-
-    return (
-        <div className='Registro'>
-            <h2>REGISTRO</h2>
-            <div className="formRegistro">
-                <form onSubmit={handleSubmit}>
-                    <input className='campos' type="text" name="nombre" value={formulario.nombre} onChange={handleChange} placeholder="Nombre" />
-                    <input className='campos' type="text" name="apellido" value={formulario.apellido} onChange={handleChange} placeholder="Apellido" />
-                    <input className='campos' type="date" name="fechaNacimiento" value={formulario.fechaNacimiento} onChange={handleChange} placeholder="Fecha de Nacimiento" />
-                    <input className='campos' type="text" name="dni" value={formulario.dni} onChange={handleChange} placeholder="DNI" />
-                    <input className='campos' type="text" name="telefono" value={formulario.telefono} onChange={handleChange} placeholder="Teléfono" />
-                    <input className='campos' type="email" name="correo" value={formulario.correo} onChange={handleChange} placeholder="Correo Electrónico" />
-                    <input className='campos' type="password" name="contraseña" value={formulario.contraseña} onChange={handleChange} placeholder="Contraseña" />
-                    <input className='campos' type="password" name="repetirContraseña" value={formulario.repetirContraseña} onChange={handleChange} placeholder="Repetir Contraseña" />
-                    <button type="submit">Registrarse</button>
-                </form>
-            </div>
-        </div>
-    );
+type UserSubmitForm = {
+	nombre: string,
+	apellido: string,
+	fechaNacimiento: Date,
+	DNI: string,
+	tel: string,
+	email: string,
+	contrasena: string,
+	repetirContrasena: string
 };
 
-export default Registro;
+export default function Registro() {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+		} = useForm<UserSubmitForm>();
+
+	const contrasena = useRef({});
+	contrasena.current = watch("contrasena", "");
+	
+	const navigate = useNavigate();
+	
+	async function onSubmit(data: UserSubmitForm) {
+		try{
+			const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.contrasena);
+			const user = userCredential.user;
+			localStorage.setItem('token', (await user.getIdToken()).toString());
+			localStorage.setItem('user', JSON.stringify(user));
+			navigate("/");
+		} 
+		catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<div className="registro">
+			<h2>Registro</h2>
+			<form className="formRegistro" onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<label className="label">Nombre</label>
+					<input type="text" placeholder="Nombre" {...register("nombre", {required: "Este campo es requerido.", maxLength: {value: 50, message: "No debe ser mayor de 50 caracteres."}})} />
+					{errors.nombre && <span className="error">{errors?.nombre?.message}</span>}
+				</div>
+				<div>
+					<label className="label">Apellido</label>
+					<input type="text" placeholder="Apellido" {...register("apellido", {required: "Este campo es requerido.", maxLength: {value: 50, message: "No debe ser mayor de 50 caracteres."}})} />
+					{errors.apellido && <span className="error">{errors?.apellido?.message}</span>}
+				</div>
+				<div>
+					<label className="label">Fecha de Nacimiento</label>
+					<input type="date" placeholder="Fecha de Nacimiento" {...register("fechaNacimiento", {required: "Este campo es requerido."})} />
+					{errors.fechaNacimiento && <span className="error">{errors?.fechaNacimiento?.message}</span>}
+				</div>
+				<div>
+					<label className="label">D.N.I.</label>
+					<input type="number" placeholder="D.N.I." {...register("DNI", {required: "Este campo es requerido.", min: {value: 10000000, message: "No debe ser menor de 8 caracteres."}, max: {value: 99999999, message: "No debe ser mayor de 8 caracteres."}})} />
+					{errors.DNI && <span className="error">{errors?.DNI?.message}</span>}
+				</div>
+				
+				<div>
+					<label className="label">Teléfono</label>
+					<input type="text" placeholder="Teléfono" {...register("tel", {})} />
+				</div>
+				<div>
+					<label className="label">Email</label>
+					<input type="email" placeholder="Email" {...register("email", {required: "Este campo es requerido."})} />
+					{errors.email && <span className="error">{errors?.email?.message}</span>}
+				</div>
+				<div>
+					<label className="label">Contraseña</label>
+					<input type="password" placeholder="Contraseña" {...register("contrasena", {required: "Este campo es requerido.", minLength: {value: 8, message: "No debe ser menor de 8 caracteres."}, maxLength: {value: 50, message: "No debe ser mayor de 50 caracteres."}})} />
+					{errors.contrasena && <span className="error">{errors?.contrasena?.message}</span>}
+				</div>
+				<div>
+					<label className="label">Repetir Contraseña</label>
+					<input type="password" placeholder="Repetir Contraseña" {...register("repetirContrasena", {required: "Este campo es requerido.", minLength: {value: 8, message: "No debe ser menor de 8 caracteres."}, maxLength: {value: 50, message: "No debe ser mayor de 50 caracteres."}, validate: value => value === contrasena.current || "Las contraseñas son diferentes."})} />
+					{errors.repetirContrasena && <span className="error">{errors?.repetirContrasena?.message}</span>}
+				</div>
+
+				<input className="btnRegistrarse" type="submit" value="Registrarse"/>
+			</form>
+		</div>
+	);
+}

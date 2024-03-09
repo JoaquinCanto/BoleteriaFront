94bff9fc02c4dea8bae7a9b51526ea65e1df10c8
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-09 19:47:52 -0300

    Toma de eventos desde el backend.

--- src/components/Eventos/Evento.tsx
+++ src/components/Eventos/Evento.tsx
@@ -1,26 +1,26 @@
 import './Evento.css'
 
-interface propTypes{
-	evento: string,
+interface propTypes {
+	nombre: string,
+	banda: string,
 	fecha: string,
-	horario: string,
+	hora: string,
 	descripcion: string
 }
 
-export function Evento(props: propTypes){
-	return(
+export function Evento(props: propTypes) {
+	return (
 		<div className="evento">
 			<div className="eventoText">
-				<span className='eventoNombre' >{props.evento}</span>
+				<div className='eventoNombre' >{props.nombre}</div>
+				<div>{props.banda}</div>
 				<div className="fechaHorario">
 					<span className="Fecha">{props.fecha}</span>
-					<span className="Horario">{props.horario}</span>
+					<span className="Horario">{props.hora}</span>
 				</div>
-				<div>
-					{props.descripcion}
-				</div>
+				<div>{props.descripcion}</div>
 			</div>
-			<img className="imgRecital" src="" alt="Imagen Recital"/>
+			<img className="imgRecital" src="" alt="Imagen Recital" />
 		</div>
 	)
 }
--- src/components/Pages/Eventos.tsx
+++ src/components/Pages/Eventos.tsx
@@ -1,13 +1,50 @@
 import './Eventos.css';
 import { Evento } from "../Eventos/Evento";
+import { signOut } from 'firebase/auth';
+import { auth } from '../../helpers/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function Eventos() {
+
+	const navigate = useNavigate();
+
+	const handleLogOut = async () => {
+		await signOut(auth);
+		localStorage.removeItem('token');
+		localStorage.removeItem('user');
+		navigate("/registrarse");
+	}
+
+	const [eventos, setEventos] = useState([]);
+
+	const fetchEventos = () => {
+		fetch("http://localhost:3000/eventos")
+			.then((response) => response.json())
+			.then((response) => {
+				setEventos(response.data);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+			});
+	}
+
+	useEffect(() => {
+		fetchEventos();
+	}, [])
+
+	// console.log(eventos);
 	return (
 		<div className="eventos">
-			<Evento evento="Recital1" fecha="10/08/24" horario="18hs"  />
-			<Evento evento="Recital2" fecha="11/08/24" horario="18hs"  />
-			<Evento evento="Recital3" fecha="12/08/24" horario="18hs"  />
-			<Evento evento="Recital4" fecha="13/08/24" horario="18hs"  />
+			{eventos.map(evento => {
+				return (
+					<Evento key={evento._id} nombre={evento.nombre} banda={evento.banda} fecha={evento.fecha} hora={evento.hora} descripcion={evento.descripcion} />
+				)
+			})}
+
+			<div>
+				<button onClick={handleLogOut}>Log out</button>
+			</div>
 		</div>
 	)
 }

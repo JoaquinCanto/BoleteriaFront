615ae524bc1fa2860917bd50bd36ae571a3bfe1d
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-12 07:37:31 -0300

    Modificaciones a las paginas de Login, Register y Usuario.

--- src/components/Usuarios/LogIn.tsx
+++ src/components/Usuarios/LogIn.tsx
@@ -1,49 +1,70 @@
-import './Usuario.css'
-import React, { useState } from 'react';
+import './Usuario.css';
+import { useForm } from 'react-hook-form';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../helpers/firebase';
+import { Link, useNavigate } from 'react-router-dom';
 
-const LogIn: React.FC = () => {
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: ''
-    });
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCredentials({
-            ...credentials,
-            [event.target.name]: event.target.value
-        });
-    };
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Aquí puedes realizar cualquier acción necesaria con las credenciales enviadas
-        console.log('Credenciales enviadas:', credentials);
-    };
-
-    return (
-        <div className="LogIn">
-            <h2>Iniciar sesión</h2>
-            <form onSubmit={handleSubmit} className="formLogIn">
-                <input
-                    type="email"
-                    name="email"
-                    value={credentials.email}
-                    onChange={handleChange}
-                    placeholder="Correo electrónico"
-                    required
-                />
-                <input
-                    type="password"
-                    name="password"
-                    value={credentials.password}
-                    onChange={handleChange}
-                    placeholder="Contraseña"
-                    required
-                />
-                <button type="submit">Ingresar</button>
-            </form>
-        </div>
-    );
+type UserLogInForm = {
+	email: string,
+	contrasena: string,
 };
 
-export default LogIn;
+export default function LogIn() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<UserLogInForm>();
+
+	const navigate = useNavigate();
+
+	async function onSubmit(login: UserLogInForm) {
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, login.email, login.contrasena);
+			const user = userCredential.user;
+			localStorage.setItem('token', (await user.getIdToken()).toString());
+			localStorage.setItem('user', JSON.stringify(user));
+			localStorage.setItem('authUser', "true");
+
+			await fetch(`http://localhost:3000/usuarios/email/${login.email}`)
+				.then(response => {
+					if (!response.ok) {
+						throw new Error('Network response was not ok');
+					}
+					return response.json();
+				})
+				.then(response => {
+					localStorage.setItem('idMongo', response.data._id)
+				})
+				.catch((error) => {
+					console.error('Error fetching data:', error);
+				});
+			navigate("/");
+		}
+		catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<div className="LogIn">
+			<h2>Log In</h2>
+			<form className="formLogIn" onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<label className="label">Email</label>
+					<input className="campo" type="email" placeholder="Email" {...register("email", { required: "Este campo es requerido." })} />
+					{errors.email && <span className="error">{errors?.email?.message}</span>}
+				</div>
+				<div>
+					<label className="label">Contraseña</label>
+					<input className="campo" type="password" placeholder="Contraseña" {...register("contrasena", { required: "Este campo es requerido.", minLength: { value: 8, message: "No debe ser menor de 8 caracteres." }, maxLength: { value: 50, message: "No debe ser mayor de 50 caracteres." } })} />
+					{errors.contrasena && <span className="error">{errors?.contrasena?.message}</span>}
+				</div>
+
+				<input className="btnLogIn" type="submit" value="Log In" />
+			</form>
+
+			<div><Link to={'/registrarse'}>¿No tienes cuenta? ¡Registrate aquí!</Link></div>
+		</div>
+	);
+}
--- src/components/Usuarios/Usuario.tsx
+++ src/components/Usuarios/Usuario.tsx
@@ -1,11 +1,60 @@
+import { useEffect, useState } from 'react';
 import './Usuario.css'
+import { useNavigate } from 'react-router-dom';
+
+type user = {
+	nombre: string,
+	apellido: string,
+	fechaNacimiento: Date,
+	DNI: string,
+	tel: string,
+	email: string,
+	admin?: boolean,
+};
 
 export default function Usuario() {
+	const navigate = useNavigate();
+
+	const [showRecitales, setShowRecitales] = useState<boolean>();
+	const [usuario, setUsuario] = useState<user>();
+	const userMongoId = localStorage.getItem('idMongo');
+
+	const token = localStorage.getItem('token');
+
+	if (!token) {
+		navigate('/registrarse');
+	}
+
+	const fetchUsuario = () => {
+		fetch(`http://localhost:3000/usuarios/${userMongoId}`)
+			.then((response) => response.json())
+			.then((response) => {
+				setUsuario(response.data);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+			});
+	}
+
+	function isAdmin() {
+		if (usuario?.admin) {
+			setShowRecitales(true);
+		}
+		else {
+			setShowRecitales(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchUsuario();
+		isAdmin();
+	}, [])
+
 	return (
 		<div>
 			<ul>
 				<li>Cuenta</li>
-				<li>Recitales</li> {/* Solo los admins */}
+				{showRecitales && <li>Recitales</li>} {/* Solo los admins */}
 				<li>Entradas</li>
 				<li>Reclamos</li>
 			</ul>

13b1617b22677c0ce955c833dbcea13eb09573c7
zequ22 <eze22alderete@gmail.com>
2024-03-12 21:57:16 -0300

    Correcciones en Sedes

--- src/components/Sedes/Monumental/Monumental.tsx
+++ src/components/Sedes/Monumental/Monumental.tsx
@@ -8,7 +8,7 @@ function Monumental() {
         <div className='Sede1'>
             <h2>Más Monumental</h2>
             <img src={FotoMonumental} alt="FotoMonumental" />
-            <h3>A cerca de</h3>
+            <h3>Acerca de</h3>
             <p>El estadio Monumental, también conocido como el Monumental de Núñez, estadio River Plate, o por cuestiones de patrocinio estadio Mâs Monumental, es un recinto deportivo ubicado en la intersección de las avenidas Figueroa Alcorta y Udaondo del barrio de Belgrano, Buenos Aires, Argentina.5​6​ Es propiedad del Club Atlético River Plate y fue inaugurado el 26 de mayo de 1938 por el presidente Antonio Vespucio Liberti, quien decidió su construcción.
                 Cuenta con una capacidad de 84 567 espectadores, luego de las remodelaciones concluidas en 2023.
                 Es el estadio de fútbol con mayor capacidad de Argentina y de América. Es el recinto donde Argentina disputa sus partidos de local. Fue sede de cuatro finales de Copa América (1946, 1959, 1987 y 2011), y sede de la final de la Copa Mundial de la FIFA 1978, entre otros eventos trascendentes.</p>

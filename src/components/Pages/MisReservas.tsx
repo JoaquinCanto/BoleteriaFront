b79e7c7d8e1e50696031024579a93d402c055a72
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2024-03-13 04:43:26 -0300

    Reorganizacion de Pages.

--- src/components/Pages/MisReservas.tsx
+++ src/components/Pages/MisReservas.tsx
@@ -1,34 +1,63 @@
+import './Pages.css';
+import { useEffect, useState } from "react";
+import ReservasRenglon from '../Reserva/ReservaRenglon';
 
-import './MisReservas.css'; 
-
-interface propTypes {
-  evento: string;
-  fecha: string;
-  zona: string;
- 
+interface reserva {
+	_id: string,
+	idUsuario: string;
+	idEvento: string;
+	nombre: string;
+	fecha: string;
+	sede: string;
+	zona: string;
+	cantidad: number;
 }
+export default function MisReservas() {
+	const [reservas, setReservas] = useState<reserva[]>([]);
 
-export default function MiCuenta(props: propTypes) {
-    return (
-      <div className="misreservas-contenedor">
-        <h1>Mis Reservas</h1>
-  
-        <div className="campos-misreservas">
-          <label>Evento:</label>
-          <p>{props.evento}</p>
-        </div>
-  
-        <div className="campos-misreservas">
-          <label>Fecha:</label>
-          <p>{props.fecha}</p>
-        </div>
-  
-        <div className="campos-misreservas">
-          <label>Zona:</label>
-          <p>{props.zona}</p>
-        </div>
-  
-        
-      </div>
-    );
+
+	const fetchEventos = () => {
+		fetch("http://localhost:3000/reservas")
+			.then((response) => response.json())
+			.then((response) => {
+				setReservas(response.data);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+			});
+	}
+
+	useEffect(() => {
+		fetchEventos();
+	}, [])
+
+	return (
+		<div className='pagReservas'>
+			<div className='reservasTitulo'>
+				<h2>Mis Reservas</h2>
+			</div>
+
+			<div className="headerReservasRenglon">
+				<div className="headerReservasNombre">Nombre</div>
+				<div className="headerReservasFecha">Fecha</div>
+				<div className="headerReservasSede">Sede</div>
+				<div className="headerReservasZona">Zona</div>
+				<div className="headerReservasCantidad">Cantidad</div>
+				<div className='headerReservasAcciones'>Ver</div>
+			</div>
+
+			{reservas.map(reserva => (
+				<ReservasRenglon
+					key={reserva._id}
+					idUsuario={reserva.idUsuario}
+					idEvento={reserva.idEvento}
+					nombre={reserva.nombre}
+					fecha={reserva.fecha}
+					sede={reserva.sede}
+					zona={reserva.zona}
+					cantidad={reserva.cantidad}
+				/>
+			))}
+		</div>
+	)
 }
